6c7cc228af35970f088c3a7caa13508317830b64
Marek324 <hrmajo1@gmail.com>
2024-04-21 20:04:57 +0200

    Implemented htab_clear

--- htab_clear.c
+++ htab_clear.c
@@ -1,5 +1,22 @@
 #include "htab.h"
+#include "htab_private.h"
 
-void htab_clear(htab_t * t){
-    
+void htab_clear(htab_t *t)
+{
+    if (t == NULL || t->arr_size == 0)
+        return;
+
+    for (size_t i = 0; i < t->arr_size; i++)
+    {
+        free_htab_items(t->htab_items[i]);
+    }
+}
+
+void free_htab_items(struct htab_item *item)
+{
+    if (item->next != NULL)
+        free_htab_items(item->next);
+    free(item->pair->key);
+    free(item->pair);
+    free(item);
 }
