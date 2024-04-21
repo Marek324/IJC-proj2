c13fb09611bb9624c4886cea0bebff69c741f275
Marek324 <hrmajo1@gmail.com>
2024-04-21 19:17:48 +0200

    Implemented htab_find

--- htab_find.c
+++ htab_find.c
@@ -1,5 +1,23 @@
 #include "htab.h"
+#include "htab_private.h"
 
-htab_pair_t * htab_find(const htab_t * t, htab_key_t key){
-    
+htab_pair_t *htab_find(const htab_t *t, htab_key_t key)
+{
+    size_t hash = htab_hash_function(key) % t->arr_size;
+    struct htab_item *a = t->htab_items[hash];
+
+    htab_pair_t *ret = NULL;
+
+    while (a != NULL)
+    {
+
+        if (key == a->pair->key)
+        {
+            ret = a->pair;
+        }
+
+        a = a->next;
+    }
+
+    return ret;
 }
