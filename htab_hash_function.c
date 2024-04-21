6150b7a76a8c19f393ee2fdfe656be95d7bc73b1
Marek324 <hrmajo1@gmail.com>
2024-04-21 18:42:36 +0200

    Implemented hash_function

--- htab_hash_function.c
+++ htab_hash_function.c
@@ -1,5 +1,11 @@
 #include "htab.h"
+#include <stdlib.h>
+#include <stdint.h>
 
-size_t htab_hash_function(htab_key_t str){
-    
+size_t htab_hash_function(const char *str) {
+    uint32_t h=0;     // musí mít 32 bitů
+    const unsigned char *p;
+    for(p=(const unsigned char*)str; *p!='\0'; p++)
+        h = 65599*h + *p;
+    return h;
 }
