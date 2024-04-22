f2bab137f3d89b363218e3c50c2690e6f748a956
Marek324 <hrmajo1@gmail.com>
2024-04-22 02:05:12 +0200

    Implemented htab_statistics

--- htab_statistics.c
+++ htab_statistics.c
@@ -1,5 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
 #include "htab.h"
+#include "htab_private.h"
 
-void htab_statistics(const htab_t * t){
-    
+void htab_statistics(const htab_t *t)
+{
+    int max = 0;
+    int min = INT_MAX;
+    int sum = 0;
+
+    int *lens = calloc(t->arr_size, sizeof(int));
+    if (lens = NULL)
+    {
+        fprintf(stderr, "Memory allocation error\n");
+        return;
+    }
+
+    for (size_t i = 0; i < t->arr_size; i++)
+    {
+        struct htab_item *itemPtr = t->htab_items[i];
+        while (itemPtr != NULL)
+        {
+
+            lens[i]++;
+
+            itemPtr = itemPtr->next;
+        }
+
+        sum += lens[i];
+
+        if (lens[i] > max)
+            max = lens[i];
+        else if (lens[i] < min)
+            min = lens[i];
+    }
+
+    float avg = (float) sum / (float) t->arr_size;
+
+    fprintf(stderr, "Minimum length: %d\n", min);
+    fprintf(stderr, "Maximum length: %d\n", max);
+    fprintf(stderr, "Average length: %f\n", avg);
 }
